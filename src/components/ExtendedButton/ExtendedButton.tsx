683fdfad56e8ad08677df2a0f927bb79547eccd6
Iliyaz Iliyaz <iliyaz.nedjati@gmail.com>
2022-04-12 11:07:28 +0300

    init

---
+++ src/components/ExtendedButton/ExtendedButton.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@mui/material";
+import React from "react";
+
+export type ExtendedButtonProps = {
+  text?: string;
+};
+
+function ExtendedButton({ text }: ExtendedButtonProps) {
+  return <Button variant="contained">{text}</Button>;
+}
+
+export default ExtendedButton;
